f6c41f0cd88ee3937e127b84a8d3453ab83f5698
dhbryan75 <dhbryan75@gmail.com>
2024-01-08 12:53:27 +0900

    Feat: 바뀐 DB구조 적용 및 js로 변경

--- index.js
+++ index.js
@@ -8,9 +8,7 @@ const { google } = require('googleapis');
 
 Dotenv.config();
 
-// DB > MetaDB > Single DB > DoNotErase > Tasks > Single Task
-
-type NotionObject = { [key: string]: any };
+// DB > MetaDB > DB > Tasks > Task
 
 const msPerSec = 1000;
 const secPerMin = 60;
@@ -32,23 +30,23 @@ const SCOPES = ['https://www.googleapis.com/auth/calendar.events'];
 const TOKEN_PATH = path.join(Process.cwd(), 'token.json');
 const CREDENTIALS_PATH = path.join(Process.cwd(), 'credentials.json');
 
-const delay = async function(ms: number): Promise<void> {
+const delay = async function(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 };
 
-const dateToTimeString = function(date: Date): string {
+const dateToTimeString = function(date) {
     return `${date.getHours().toString().padStart(2, '0')}:${date.getMinutes().toString().padStart(2, '0')}`
 };
 
-const dateToDateString = function(date: Date): string {
+const dateToDateString = function(date) {
     return `${date.getFullYear().toString().padStart(4, '0')}/${(date.getMonth() + 1).toString().padStart(2, '0')}/${date.getDate().toString().padStart(2, '0')}`;
 }
 
-const dateToDateTimeString = function(date: Date): string {
+const dateToDateTimeString = function(date) {
     return `${dateToDateString(date)} ${dateToTimeString(date)}`;
 };
 
-const stringToDate = (dateString: string): Date | null => {
+const stringToDate = (dateString) => {
 	if (!dateString) {
 		return null;
 	}
@@ -69,7 +67,7 @@ const stringToDate = (dateString: string): Date | null => {
  *
  * @return {Promise<OAuth2Client|null>}
  */
-async function loadSavedCredentialsIfExist(): Promise<any> {
+async function loadSavedCredentialsIfExist() {
 	try {
 		const content = await fs.readFile(TOKEN_PATH);
 		const credentials = JSON.parse(content);
@@ -85,7 +83,7 @@ async function loadSavedCredentialsIfExist(): Promise<any> {
  * @param {OAuth2Client} client
  * @return {Promise<void>}
  */
-async function saveCredentials(client: any): Promise<void> {
+async function saveCredentials(client) {
 	const content = await fs.readFile(CREDENTIALS_PATH);
 	const keys = JSON.parse(content);
 	const key = keys.installed || keys.web;
@@ -102,7 +100,7 @@ async function saveCredentials(client: any): Promise<void> {
  * Load or request or authorization to call APIs.
  *
  */
-async function authorize(): Promise<any> {
+async function authorize() {
 	let client = await loadSavedCredentialsIfExist();
 	if (client) {
 		return client;
@@ -117,7 +115,7 @@ async function authorize(): Promise<any> {
 	return client;
 };
 
-const insertEvent = async(auth: any, databaseId: string, taskPageId: string, taskUrl: string, databaseName: string, taskProjectNamesString: string, taskTitle: string, taskStatus: string, taskStartDate: Date | null, taskEndDate: Date | null, taskPersonNamesString: string) => {
+const insertEvent = async(auth, databaseId, taskPageId, taskUrl, databaseName, taskProjectNamesString, taskTitle, taskStatus, taskStartDate, taskEndDate, taskPersonNamesString) => {
 	const nowStr = dateToDateTimeString(new Date());
 	const eventTitle = taskTitle;
 	const eventDescription = `[${databaseName}/${taskProjectNamesString}] Responsibility: ${taskPersonNamesString} / Status: ${taskStatus} / UpdatedAt: ${nowStr} / NotionDatabaseId: ${databaseId} / NotionPageId: ${taskPageId} / NotionPageUrl: ${taskUrl}`;
@@ -162,7 +160,7 @@ const insertEvent = async(auth: any, databaseId: string, taskPageId: string, tas
 		timeMax: searchEndDate.toISOString(),
 		timeMin: searchStartDate.toISOString(),
 	});
-	const sameEvents = searchResult.data.items.filter((event: any) => {
+	const sameEvents = searchResult.data.items.filter((event) => {
 		return event.description?.includes(taskPageId);
 	});
 	for(let event of sameEvents) {
@@ -171,7 +169,7 @@ const insertEvent = async(auth: any, databaseId: string, taskPageId: string, tas
 			auth: auth,
 			calendarId: 'primary',
 			eventId: eventId,
-		}), function (error: any, event: any) {
+		}), function (error, event) {
 			if (error) {
 				console.log(eventTitle);
 				return;
@@ -183,7 +181,7 @@ const insertEvent = async(auth: any, databaseId: string, taskPageId: string, tas
 		auth: auth,
 		calendarId: 'primary',
 		resource: event,
-	}, function (error: any, event: any) {
+	}, function (error, event) {
 		if (error) {
 			console.log(eventTitle);
 			return;
@@ -217,7 +215,7 @@ const refresh = async() => {
 			page_size: pageSize,
 		})).results;
 
-		const callout = databaseBlocks.reverse().find((block: NotionObject) => {
+		const callout = databaseBlocks.reverse().find((block) => {
 			return block.type === "callout";
 		});
 		const callOutBlocks = (await notion.blocks.children.list({
@@ -225,7 +223,7 @@ const refresh = async() => {
 			page_size: pageSize,
 		})).results;
 
-		const toggle = callOutBlocks.find((block: NotionObject) => {
+		const toggle = callOutBlocks.find((block) => {
 			return block.type === "toggle";
 		})
 		const toggleBlocks = (await notion.blocks.children.list({
@@ -233,44 +231,29 @@ const refresh = async() => {
 			page_size: pageSize,
 		})).results;
 
-		const DoNotErasePage = toggleBlocks.find((block: NotionObject) => {
-			return block.type === "child_page";
-		})
-		const DoNotErasePageBlocks = (await notion.blocks.children.list({
-			block_id: DoNotErasePage.id,
-			page_size: pageSize,
-		})).results;
-
-		const TasksDatabase = DoNotErasePageBlocks.find((block: NotionObject) => {
+		const tasksDatabase = toggleBlocks.find((block) => {
 			if (block.type === "child_database") {
 				return block.child_database.title === "Tasks";
 			}
 			return false;
 		});
+		if(!tasksDatabase) {
+			continue;
+		}
 
 		const recentFilter = {
-			"and": [
-				{
-					"timestamp": "last_edited_time",
-					"last_edited_time": {
-						"on_or_after": new Date(lastUpdateMs).toISOString().split(".")[0],
-					}
-				},
-				{
-					"timestamp": "last_edited_time",
-					"last_edited_time": {
-						"before": new Date(nowMinuteMs).toISOString().split(".")[0],
-					}
-				},
-			],
+			"timestamp": "last_edited_time",
+			"last_edited_time": {
+				"on_or_after": new Date(lastUpdateMs).toISOString().split(".")[0],
+			}
 		};
 
 		let hasMore = true;
 		let nextCursor;
 		let taskPages = [];
 		while (hasMore) {
-			const taskPagesResponse: NotionObject = await notion.databases.query({
-				database_id: TasksDatabase.id,
+			const taskPagesResponse = await notion.databases.query({
+				database_id: tasksDatabase.id,
 				filter: recentFilter,
 				page_size: pageSize,
 				start_cursor: nextCursor,
@@ -290,7 +273,7 @@ const refresh = async() => {
 			const taskStatus = taskPage.properties["상태"].status?.name;
 
 			const taskProjectPages = taskPage.properties["Projects"].relation;
-			const taskProjectNamePromises = taskProjectPages.map(async (page: NotionObject) => {
+			const taskProjectNamePromises = taskProjectPages.map(async (page) => {
 				const pageResponse = await notion.pages.retrieve({ page_id: page.id });
 				return pageResponse.properties["제목"].title[0]?.plain_text;
 			});
@@ -298,7 +281,7 @@ const refresh = async() => {
 			const taskProjectNamesString = taskProjectNames.join(", ");
 
 			const taskPersonPages = taskPage.properties["담당자"].relation;
-			const taskPersonNamePromises = taskPersonPages.map(async (page: NotionObject) => {
+			const taskPersonNamePromises = taskPersonPages.map(async (page) => {
 				const pageResponse = await notion.pages.retrieve({ page_id: page.id });
 				return pageResponse.properties["이름"].title[0]?.plain_text;
 			});
